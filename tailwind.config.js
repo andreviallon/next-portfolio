8f34134774ba2ea4e30e174ef91fa267151feddf
Andre Viallon <viallon.andre@gmail.com>
2021-05-12 17:38:40 +0200

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false,
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false,
+	theme: {
+		screens: {
+			'sm': '740px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px'
+		},
+		colors: {
+			white: '#ffffff',
+			black: '#000000',
+			gray: '5F5F5F'
+		}
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
