8303873b0fe2d153dc30d5a891639940b52ff52f
Andre Viallon <viallon.andre@gmail.com>
2021-05-14 10:26:13 +0200

    Project card styling

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -8,14 +8,14 @@ interface Props {
 const ProjectCard: React.FC<Props> = ({ project }) => {
   return (
     <div>
-      <div className="relative w-full h-full shadow-lg">
-        <Image src={`/${project.imagePath}.png`} alt="Avatar" layout="responsive" height={200} width={200} />
+      <div className="shadow-lg rounded-xl">
+        <Image src={`/${project.imagePath}.png`} alt="Avatar" layout="responsive" height={130} width={200} objectFit="cover" className="rounded-xl" />
       </div>
-      <p className="text-xl font-semibold mt-4">{project.title}</p>
-      <p className="text-gray-500 mt-2">{project.description}</p>
+      <p className="text-xl font-semibold mt-6">{project.title}</p>
+      <p className="text-gray-500 mt-4 leading-7">{project.description}</p>
       <div className="mt-4">
-        <a className="font-semibold hide-bottom-bar-on-hover">live demo</a>
-        <a className="font-semibold hide-bottom-bar-on-hover ml-6">see code</a>
+        <a target="_blank" href={project.websiteLink} rel="noopener noreferrer" className="font-semibold hide-bottom-bar-on-hover">live demo</a>
+        <a target="_blank" href={project.githubLink} rel="noopener noreferrer" className="font-semibold hide-bottom-bar-on-hover ml-6">see code</a>
       </div>
     </div>
   )
